fa86f855d81466e2f27793df30e0445f50236704
Hector Budiel E <hector6420@hotmail.com>
2021-07-19 00:31:12 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+##############################
+## AWS Provider Defaults
+##----------------------------
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {
+    default = "us-east-1"
+}
+
+##############################
+## AWS Tags
+##----------------------------
+variable "project" {}
+variable "environment" {}
+variable "owner" {}
+variable "service" {}
+variable "product" {}
+variable "portfolio" {}
+
+
+##############################
+## AWS Account Information
+##----------------------------
+variable "aws_account_id" {}
+
+##############################
+## AWS Expiration Policy
+##----------------------------
+
+variable "los_lifecycle_deletion_enabled" {
+    default = false
+}
+
+variable "los_lifecycle_deletion_time_days" {
+    default = 547
+}
