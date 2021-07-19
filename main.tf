d507d0eead09c8bcd320ca726e0acb84d139cb9b
Hector Budiel E <hector6420@hotmail.com>
2021-07-19 10:24:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ provider "aws" {
     secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
 }
+
+locals {
+    local_account_id_root     = "arn:aws:iam::${var.aws_account_id}:root"
+    readonly_account_arn_list = var.readonly_account_arns != [""] ? var.readonly_account_arns : [local.local_account_id_root]
+}
